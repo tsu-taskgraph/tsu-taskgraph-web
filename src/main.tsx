a61157491ffc765b6a7267c591f711a5ca31474c
Nikita Lugachev <nikitalugachev149th@gmail.com>
2026-06-14 14:25:47 +0700

    Integrate Grafana Faro for Grafana Logs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FaroErrorBoundary } from '@grafana/faro-react'
+import { initFaro } from './faro'
 import './index.css'
 import App from './App.tsx'
+
+initFaro();
 
 async function enableMocking() {
   if (!import.meta.env.DEV) {
@@ -16,7 +20,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <App />
+      <FaroErrorBoundary>
+        <App />
+      </FaroErrorBoundary>
     </StrictMode>,
   )
 });
